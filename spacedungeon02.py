5f8c0c4119abc0f70b0a9fe0997947f131e559cb
s203 <s203@AI241LAB0203M19.local>
2017-11-08 12:05:28 -0800

    changes

---
+++ spacedungeon02.py
@@ -0,0 +1,32 @@
+# mp = 50
+#
+# while (mp > 0) == True:
+#     # put instructions here
+#     print("You cast a spell")
+#     # subtract from mp
+#     # show user how many mp remain
+#     mp -= 10
+#     print("You have ", mp, "magic points")
+#
+# hp = 50
+#
+# for (hp 10 in range(5))
+# print ("i have ", hp, "points ")
+#
+#
+#
+#
+# import random
+
+
+hp = 50
+mp = 50
+
+while (mp > 0) == True:
+    print("you have", mp, " magic points")
+    mp -= 10
+
+for attack in range(5):
+    print(hp)
+    print("You were attacked by a were-whale", 10, " damage.")
+    hp -= 10
