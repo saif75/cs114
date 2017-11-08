d326d019406fe97524aded38fe3d25fea05d5518
s203 <s203@AI241LAB0203M19.local>
2017-11-08 10:17:22 -0800

    changes

---
+++ s.py
@@ -0,0 +1,13 @@
+print("this is a game")
+print("what do you want to name your character?")
+name = input()
+print("What character type?")
+character = input()
+print ("what level you want?")
+level = input()
+print ("chose an acter")
+acter = input()
+#other stuff here
+
+print("ok " + name + " you have chosen " + character + " blah blah" + level + " good ")
+print ("have fun ")
